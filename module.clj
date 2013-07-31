9bf85d2ac8413a561df03d957925c5e33cd3b68a
Ludger Solbach <soulman@soulspace.org>
2013-07-31 10:07:13 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -4,6 +4,6 @@
  :type :library
  :version "0.2.0"
  :description "Clojure math library"
- :plugins ["global" "sdeps" "clojure" "package"]
+ :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]]
  ]
