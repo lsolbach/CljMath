cbada7afbaa866d6697204aff5e6e13a6111e9c0
Ludger Solbach <soulman@soulspace.org>
2013-08-05 02:42:59 +0200

    updated module files, added copyright notice in source files

--- module.clj
+++ module.clj
@@ -4,6 +4,6 @@
  :type :library
  :version "0.2.0"
  :description "Clojure math library"
- :plugins ["global" "sdeps" "depsdot" "clojure" "package"]
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [[["org.clojure" "clojure" "1.5.1"]]]
  ]
