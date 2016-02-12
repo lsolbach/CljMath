f735a629958228c0cc31ab5f326d91e21d411986
Ludger Solbach <soulman@soulspace.org>
2016-02-12 08:47:06 +0100

    added probability functions
    added inverse-normal-cdf, bernoulli and binomial

--- CljMathLibrary/src/org/soulspace/clj/math/probability.clj
+++ CljMathLibrary/src/org/soulspace/clj/math/probability.clj
@@ -18,7 +18,7 @@
     :else 1)))
 
 (defn normal-pdf
-  "Returns the probability density function for the uniform distribution."
+  "Returns the probability density function for the normal distribution."
   ([x]
     (normal-pdf x 0 1))
   ([x mu sigma]
@@ -27,10 +27,31 @@
        (sqrt (* 2 pi)))))
 
 (defn normal-cdf
-  "Returns the cumulative distribution function for the uniform distribution."
+  "Returns the cumulative distribution function for the normal distribution."
   ([x]
     (normal-cdf x 0 1))
   ([x mu sigma]
     (/ (+ 1 (erf (/ (- x mu)
                     (* (sqrt 2) sigma))))
        2)))
+
+(defn inverse-normal-cdf
+  "Returns the inverse of the cumulative distribution function for the normal distribution. Approximates the value with binary search."
+  ([p]
+    (inverse-normal-cdf p 0.00001))
+  ([p e]
+    (search-value normal-cdf p -10.0 10.0 e))
+  ([p mu sigma e]
+    (+ mu (* sigma (inverse-normal-cdf p 0.00001)))))
+
+(defn bernoulli
+  "Calculates the bernulli(p) random variable. Returns 1 with a probability of p and 0 with a probability of (1 - p)."
+  [p]
+  (if (< (rand) p)
+    1
+    0))
+
+(defn binomial
+  "Calculates the binomial(n, p) random variable."
+  [n p]
+  (reduce + 0 (take n (repeatedly #(bernoulli p)))))
