c69d0fe9b808b0b0d4d1506175ba87c5f4bebb55
Ludger Solbach <soulman@soulspace.org>
2016-02-16 00:10:52 +0100

    implemented vector functions on clj vectors

--- CljMathLibrary/src/org/soulspace/clj/math/matrix.clj
+++ CljMathLibrary/src/org/soulspace/clj/math/matrix.clj
@@ -9,38 +9,30 @@
 ;
 (ns org.soulspace.clj.math.matrix)
 
-(defprotocol Matrix
-  "Protocol for matrices."
-  (element [m i j] "Returns the element of the matrix m at row i and column j.")
-  (row-vector [m i] "Returns the row vector of the matrix m at row i.")
-  (column-vector [m j] "Returns the column vector of the matrix m at column j.")
-  (upper-triangular [m] "Returns the upper triangular of the matrix m.")
-  (lower-triangular [m] "Returns the upper triangular of the matrix m.")
-  (scalar-sum [m s] "Calculates the scalar sum of the matrix m with the scalar s.")
-  (scalar-product [m s] "Calculates the scalar product of the matrix m with the scalar s.")
-  (matrix-sum [m m2] "Calculates the sum of the matrix m with the matrix m2.")
-  (matrix-product [m m2] "")
-  (transpose [m] "Returns the transposed matrix of the matrix m.")
-  (solve [m v] ""))
-
-(defrecord VecMatrixImpl
-  [elements]
-  Matrix
-  (element [m i j])
-  (row-vector [m i])
-  (column-vector [m j])
-  (scalar-sum [m r])
-  (scalar-product [m r])
-  (matrix-sum [m m2])
-  (matrix-product [m m2])
-  (upper-triangular [m])
-  (lower-triangular [m])
-  (transpose [m])
-  (solve [m v]))
-
-(defn get-element
+(defn element
+  "Returns the element of the matrix m at row i and column j."
   [m i j]
   (nth (nth m i) j))
+
+(defn row-vector
+  "Returns the row vector of the matrix m at row i."
+  [m i]
+  (nth m i))
+
+(defn column-vector
+  "Returns the column vector of the matrix m at column j."
+  [m j]
+  (for [i ]))
+
+(defn scalar-add
+  "Adds the scalar s to the matrix m."
+  [m r]
+  (for []))
+
+(defn scalar-multiply
+  "Multiplies the scalar s to the matrix m."
+  [m r]
+  )
 
 (defn matrix-sum
   [m m2]
@@ -65,3 +57,39 @@
 (defn lower-triangular
   [m]
   )
+
+(comment
+(defprotocol Matrix
+  "Protocol for matrices."
+  (element [m i j] "Returns the element of the matrix m at row i and column j.")
+  (row-vector [m i] "Returns the row vector of the matrix m at row i.")
+  (column-vector [m j] "Returns the column vector of the matrix m at column j.")
+  (upper-triangular [m] "Returns the upper triangular of the matrix m.")
+  (lower-triangular [m] "Returns the upper triangular of the matrix m.")
+  (scalar-sum [m s] "Calculates the scalar sum of the matrix m with the scalar s.")
+  (scalar-product [m s] "Calculates the scalar product of the matrix m with the scalar s.")
+  (matrix-sum [m m2] "Calculates the sum of the matrix m with the matrix m2.")
+  (matrix-product [m m2] "")
+  (transpose [m] "Returns the transposed matrix of the matrix m.")
+  (solve [m v] ""))
+
+(defrecord VecMatrixImpl
+  [elements]
+  Matrix
+  (element [m i j]
+    (nth (nth m i) j))
+  (row-vector [m i]
+    (nth m i))
+  (column-vector [m j]
+    (for [i ]))
+  (scalar-sum [m r]
+    )
+  (scalar-product [m r])
+  (matrix-sum [m m2])
+  (matrix-product [m m2])
+  (upper-triangular [m])
+  (lower-triangular [m])
+  (transpose [m])
+  (solve [m v]))
+)
+
--- CljMathLibrary/src/org/soulspace/clj/math/vector.clj
+++ CljMathLibrary/src/org/soulspace/clj/math/vector.clj
@@ -7,51 +7,90 @@
 ;   the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
 ;
-(ns org.soulspace.clj.math.vector)
+(ns org.soulspace.clj.math.vector
+  (:use [org.soulspace.clj.math java-math]))
 
-(defprotocol Vector
-  "Protocol for vectors."
-  (scalar-add v s)
-  (scalar-product v s)
-  (vector-add v v)
-  )
+(def vector-dimension
+  "Returns the dimension of the vector v."
+  count)
 
-(defrecord VecVectorImpl
-  [elements]
-  Vector
-  (scalar-add v s)
-  (scalar-product v s)
-  (vector-add v v)
-  )
+(defn scalar-add
+  "Adds a scalar s to the vector v."
+  [s v]
+  (mapv #(+ % s) v))
 
-(defn addition
+(defn scalar-multiply
+  "Multiplies a scalar s to the vector v."
+  [s v]
+  (mapv #(* % s) v))
+
+(defn vector-add
   "Adds the vectors."
-  [a b]
-  (if (= (count a) (count b))
-    (vec (map + a b))
-    (throw (IllegalArgumentException. "The vectors are not of the same size."))))
+  [v w]
+  (if (= (count v) (count w))
+    (mapv + v w)
+    (throw (IllegalArgumentException. "The vectors are not of the same dimension."))))
 
-(defn substraction
-  "Substracts the vectors."
-  [a b]
-  (if (= (count a) (count b))
-    (vec (map - a b))
-    (throw (IllegalArgumentException. "Vectors are not of the same size."))))
+(defn vector-substract
+  "Substracts the vectors v and w."
+  [v w]
+  (if (= (count v) (count w))
+    (mapv - v w)
+    (throw (IllegalArgumentException. "The Vectors are not of the same dimension."))))
 
-(defn scalar-product
-  "Returns the scalar product of the vectors."
-  [r a]
-  (vec (map #(* r %) a)))
+(defn vector-sum
+  "Adds the vectors."
+  [& vs]
+  (reduce vector-add vs))
 
 (defn dot-product
-  "Returns the dot product of the vectors."
-  [a b]
-  (if (= (count a) (count b))
-    (reduce + (map * a b))
-    (throw (IllegalArgumentException. "Vectors are not of the same size."))))
+  "Returns the dot product of the vectors v and w."
+  [v w]
+  (if (= (count v) (count w))
+    (reduce + (map * v w))
+    (throw (IllegalArgumentException. "The Vectors are not of the same dimension."))))
+
+(defn sum-of-squares
+  "Returns the sum of the squares of v."
+  [v]
+  (dot-product v v))
+
+(defn magnitude
+  "Returns the magnitude of the vector v."
+  [v]
+  (sqrt (sum-of-squares v)))
+
+(defn distance
+  "Returns the distance of the vectors v and w."
+  [v w]
+  (magnitude (vector-substract v w)))
+
+(defn normalize
+  "Returns a vector with a length of 1 in the direction of v."
+  [v]
+  (scalar-multiply (/ 1 (magnitude v)) v))
 
 (defn cross-product
   [a b]
   (if (= (count a) (count b))
     nil ; TODO calculate cross product
-    (throw (IllegalArgumentException. "Vectors are not of the same size."))))
+    (throw (IllegalArgumentException. "The Vectors are not of the same dimension."))))
+
+(comment
+(defprotocol Vector
+  "Protocol for vectors."
+  (scalar-add [v s])
+  (scalar-product [v s])
+  (vector-add [v v2])
+  (vector-product [v v2])
+  )
+
+(defrecord VecVectorImpl
+  [elements]
+  Vector
+  (scalar-add [v s])
+  (scalar-product [v s])
+  (vector-add [v v2])
+  (vector-product [v v2])
+  )
+)
