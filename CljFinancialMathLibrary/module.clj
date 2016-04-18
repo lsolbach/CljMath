decb99667529e2288f2915133a22c2a61619fd9d
Ludger Solbach <soulman@soulspace.org>
2016-04-18 21:36:52 +0200

    updated clojure version

--- CljFinancialMathLibrary/module.clj
+++ CljFinancialMathLibrary/module.clj
@@ -2,12 +2,12 @@
  :module "CljFinancialMathLibrary"
  :project "org.soulspace.clj"
  :type :library
- :version "0.3.0"
+ :version "0.4.0"
  :description "Clojure financial math library"
  :plugins ["global"
            ["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
-                ["org.soulspace.clj/CljMathLibrary, 0.3.0"]]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
+                ["org.soulspace.clj/CljMathLibrary, 0.5.0"]]
  ]
--- CljOctonionMathLibrary/module.clj
+++ CljOctonionMathLibrary/module.clj
@@ -8,6 +8,6 @@
            ["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.7.0"]
-                ["org.soulspace.clj/CljMathLibrary, 0.3.0"]]
+ :dependencies [["org.clojure/clojure, 1.8.0"]
+                ["org.soulspace.clj/CljMathLibrary, 0.5.0"]]
  ]
