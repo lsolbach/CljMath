298d522f7b4801281fbe00f42a9ecb79a168faca
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2016-04-19 20:41:38 +0200

    removed global plugin from module.clj

--- CljFinancialMathLibrary/module.clj
+++ CljFinancialMathLibrary/module.clj
@@ -4,8 +4,7 @@
  :type :library
  :version "0.4.0"
  :description "Clojure financial math library"
- :plugins ["global"
-           ["org.soulspace.baumeister/ClojurePlugin"]
+ :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.clojure/clojure, 1.8.0"]
--- CljOctonionMathLibrary/module.clj
+++ CljOctonionMathLibrary/module.clj
@@ -4,8 +4,7 @@
  :type :library
  :version "0.3.0"
  :description "Clojure math library"
- :plugins ["global"
-           ["org.soulspace.baumeister/ClojurePlugin"]
+ :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.clojure/clojure, 1.8.0"]
