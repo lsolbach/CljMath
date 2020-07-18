b6c695234341003f5dd61694a5ac9bd3e2cc7c94
Ludger Solbach <soulman@soulspace.org>
2020-07-18 18:00:27 +0200

    added geometry tests, updated clojure version to 1.10.1

--- CljQuaternionMathLibrary/module.clj
+++ CljQuaternionMathLibrary/module.clj
@@ -8,5 +8,5 @@
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/ClojureTestPlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
- :dependencies [["org.clojure/clojure, 1.8.0"]
+ :dependencies [["org.clojure/clojure, 1.10.1"]
                 ["org.soulspace.clj/CljMathLibrary, 0.5.0"]]]
