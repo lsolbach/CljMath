f16327a400ba1166c93d21fb847428c3abb21a70
Ludger Solbach <soulman@soulspace.org>
2020-08-01 04:11:43 +0200

    refacored namespaces

--- math.base/project.clj
+++ math.base/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :test-paths ["unittest"])
+  :test-paths ["test"])
